6f4a960e568cc8d5fc47661372192b024906a0cc
muhlinux <41151600+muhlinux@users.noreply.github.com>
2019-10-06 16:03:49 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,27 +8,27 @@ ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 hwclock --systohc
 
 echo ":: Configuring localization..."
-locale_gen=("en_US.UTF-8 UTF-8" \
+locale_gen=("en_US.UTF-8 UTF-8"
             "en_DK.UTF-8 UTF-8")
 printf '%s\n' "${locale_gen[@]}" > /etc/locale.gen && locale-gen
 
 LANG="en_DK.UTF-8"
-locale_conf=("LANG=${LANG}" \
-             "LC_ADDRESS=${LANG}" \
-             "LC_IDENTIFICATION=${LANG}" \
-             "LC_MEASUREMENT=${LANG}" \
-             "LC_MONETARY=${LANG}" \
-             "LC_NAME=${LANG}" \
-             "LC_NUMERIC=${LANG}" \
-             "LC_PAPER=${LANG}" \
-             "LC_TELEPHONE=${LANG}" \
+locale_conf=("LANG=${LANG}"
+             "LC_ADDRESS=${LANG}"
+             "LC_IDENTIFICATION=${LANG}"
+             "LC_MEASUREMENT=${LANG}"
+             "LC_MONETARY=${LANG}"
+             "LC_NAME=${LANG}"
+             "LC_NUMERIC=${LANG}"
+             "LC_PAPER=${LANG}"
+             "LC_TELEPHONE=${LANG}"
              "LC_TIME=${LANG}")
 printf '%s\n' "${locale_conf[@]}" > /etc/locale.conf
 echo "KEYMAP=dk-latin1" > /etc/vconsole.conf
 
 echo ":: Configuring networking..."
 read -r -p "Choose a hostname: " HOSTNAME 
-hosts=("127.0.0.1   localhost" \
+hosts=("127.0.0.1   localhost"
        "::1         localhost"
        "127.0.1.1	${HOSTNAME}.localdomain	${HOSTNAME}")
 printf '%s\n' "${hosts[@]}" > /etc/hosts && echo "$HOSTNAME" > /etc/hostname
